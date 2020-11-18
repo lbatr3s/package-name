2009e21e856865bdf1b1f46be9ae381e2ae1372f
Lester Batres <lester@soyfri.com>
2020-11-18 11:34:10 -0600

    Initial commit

---
+++ Sources/ModuleName/First.swift
@@ -0,0 +1,5 @@
+public struct First {
+    public var text = "Hello, World!"
+    
+    public init() {}
+}
---
+++ Sources/SecondModuleName/Second.swift
@@ -0,0 +1,8 @@
+import ModuleName
+
+public struct Second {
+    public var text = "Goodbye, World!"
+    public var moreText = First().text
+    
+    public init() {}
+}
